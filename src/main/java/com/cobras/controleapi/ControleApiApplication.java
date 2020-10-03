31c0eb1908cffec07bccd45cc9ba560a80729e30
Ulisses Douglas <ulisses.portela@hmail.com>
2020-10-03 12:35:38 -0300

    Criando o projeto

---
+++ src/main/java/com/cobras/controleapi/ControleApiApplication.java
@@ -0,0 +1,13 @@
+package com.cobras.controleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cobras/controleapi/ControleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobras.controleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
