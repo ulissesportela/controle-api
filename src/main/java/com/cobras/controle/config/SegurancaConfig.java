c2fb4a28edeb9e60e3771a0842865370c6ea631b
leo <leo.araujo.pereira@gmail.com>
2020-10-28 11:37:03 -0300

    Aut config id app token maxage

--- src/main/java/com/cobras/controle/config/SegurancaConfig.java
+++ src/main/java/com/cobras/controle/config/SegurancaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -38,9 +39,10 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 	   @Override
 	    public void configure(HttpSecurity http) throws Exception {
 	        http
-	            .csrf().disable()
-	            .cors().disable()
-	            .authorizeRequests().antMatchers("/unidades*/","/unidades/**").permitAll().antMatchers("/unidades*/","/unidades/**").authenticated().anyRequest().denyAll();
+            .csrf().disable()
+            .cors()
+        .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 	    }
 
