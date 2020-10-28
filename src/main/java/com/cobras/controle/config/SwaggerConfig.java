df4558fcf92385431f4227875cd7dd3905d288e6
leo <leo.araujo.pereira@gmail.com>
2020-10-28 14:27:19 -0300

    correcao do swagger para webmvc e documentacao em pdf dos servicos Rest

--- src/main/java/com/cobras/controle/config/SwaggerConfig.java
+++ src/main/java/com/cobras/controle/config/SwaggerConfig.java
@@ -1,39 +1,47 @@
 package com.cobras.controle.config;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-
+public class SwaggerConfig implements WebMvcConfigurer {
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.cobras.controle.api.controller"))
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(apiInfo());
+				.paths(PathSelectors.any()).build().apiInfo(metaData());
 	}
 
-	private ApiInfo apiInfo() {
-		return new ApiInfo(
-				"Rest Api - Controle", 
-				"Descricao da API.",
-				"v1",
-				"Termos de Servico Url",
-				new Contact("controle", "www.controle.com.br", "controle@gmail.com"),
-				"Licenca da API", "Licenca da URL", Collections.emptyList());
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("Spring Boot Swagger App").description("\"Spring Boot Swagger Server App\"")
+				.version("1.0.0").license("Apache License Version 2.0")
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"").build();
 	}
+
+	public ApiInfo apiInfo() {
+		final ApiInfoBuilder builder = new ApiInfoBuilder();
+		builder.title("Swagger Test App").version("1.0").license("(C) Copyright Test")
+				.description("The API provides a platform to query build test swagger api");
+
+		return builder.build();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
 }
