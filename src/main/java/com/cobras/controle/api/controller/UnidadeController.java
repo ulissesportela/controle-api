cfb3dc7f9adcfca4359da317f201b4471d9cd00c
leo <leo.araujo.pereira@gmail.com>
2020-10-28 11:02:49 -0300

    teste

--- src/main/java/com/cobras/controle/api/controller/UnidadeController.java
+++ src/main/java/com/cobras/controle/api/controller/UnidadeController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "Endpoint de Unidades")
 @RestController
 @RequestMapping("/unidades")
+@CrossOrigin(origins = "*", maxAge = 86400, allowedHeaders = "*")
 public class UnidadeController {
 
 	@Autowired
