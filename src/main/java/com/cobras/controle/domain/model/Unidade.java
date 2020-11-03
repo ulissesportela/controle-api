90e7dad7f36ec9c518a5f3b2a5cd73155802219e
leo <leo.araujo.pereira@gmail.com>
2020-11-03 19:43:54 -0300

    telefone

--- src/main/java/com/cobras/controle/domain/model/Unidade.java
+++ src/main/java/com/cobras/controle/domain/model/Unidade.java
@@ -13,12 +13,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.validation.Constraint;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
