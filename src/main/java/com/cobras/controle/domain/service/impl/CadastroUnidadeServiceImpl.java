bb7356433f06ebd2db7e0243ecffb4d2f1a3751a
leo <leo.araujo.pereira@gmail.com>
2020-11-04 03:23:23 -0300

    voltando o codigo, testado todas as condicoes possiveis no insomnia

--- src/main/java/com/cobras/controle/domain/service/impl/CadastroUnidadeServiceImpl.java
+++ src/main/java/com/cobras/controle/domain/service/impl/CadastroUnidadeServiceImpl.java
@@ -24,10 +24,9 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 
 	@Autowired
 	private UnidadeRepository unidadeRepository;
-	
+
 	public Unidade incluir(Unidade unidade) {
 		Unidade unidadeExistente = unidadeRepository.findByCodigo(unidade.getCodigo());
-
 
 		if (unidadeExistente != null) {
 			throw new NegocioException("Já existe uma unidade cadastrada com esse código");
@@ -41,7 +40,7 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 		if (unidadeExistente != null && !unidadeExistente.getId().equals(unidade.getId())) {
 			throw new NegocioException("Já existe uma unidade cadastrada com esse código");
 		}
-		if(unidade.getTelefone() != null) {
+		if (unidade.getTelefone() != null) {
 			unidade.setTelefone(unidade.getTelefone().replaceAll("[\\s()-]", "").replace(" ", "").trim());
 		}
 		return unidadeRepository.save(unidade);
@@ -62,12 +61,9 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 		return unidadeRepository.findByEmail(email);
 	}
 
-
 	@Override
-	public List<Unidade> findByCodigoAndNomeAndResponsavel(String codigo, 
-			String nome, String responsavel) {
-		return unidadeRepository.findByCodigoAndNomeAndResponsavel(codigo, nome, 
-				responsavel);
+	public List<Unidade> findByCodigoAndNomeAndResponsavel(String codigo, String nome, String responsavel) {
+		return unidadeRepository.findByCodigoAndNomeAndResponsavel(codigo, nome, responsavel);
 	}
 
 	@Override
@@ -76,10 +72,9 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 	}
 
 	@Override
-	public List<Unidade> findByCodigoAndNomeAndResponsavelAndAtivo(String codigo, String nome,
-			String responsavel, char ativo) {
-		return unidadeRepository.findByCodigoAndNomeAndResponsavelAndAtivo(codigo, nome, 
-				responsavel, ativo);
+	public List<Unidade> findByCodigoAndNomeAndResponsavelAndAtivo(String codigo, String nome, String responsavel,
+			char ativo) {
+		return unidadeRepository.findByCodigoAndNomeAndResponsavelAndAtivo(codigo, nome, responsavel, ativo);
 	}
 
 	@Override
@@ -98,10 +93,9 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 	}
 
 	@Override
-	public List<Unidade> findByListaParametros(String codigo, String nome, 
-			String responsavel, char ativo, Long cidade, Integer tipo, Long estadoId) {
-		return unidadeRepository.findByListaParametros(codigo, nome, responsavel, 
-				ativo, cidade, tipo, estadoId);
+	public List<Unidade> findByListaParametros(String codigo, String nome, String responsavel, char ativo, Long cidade,
+			Integer tipo, Long estadoId) {
+		return unidadeRepository.findByListaParametros(codigo, nome, responsavel, ativo, cidade, tipo, estadoId);
 	}
 
 	@Override
@@ -113,67 +107,44 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 	@Transactional(readOnly = true)
 	public List<Unidade> buscar(Unidade unidade) {
 		Unidade unidade2 = new Unidade();
-		if(unidade == null) {
+		unidade2.setCidade(new Municipio());
+		unidade2.getCidade().setEstado(new Estado());
+		if (unidade == null) {
 			return unidadeRepository.findAll();
 		}
-		if(unidade.getCidade() == null) {
-			unidade2.setCidade(new Municipio());
-		}
-		
-		if(unidade.getCidade() != null ) {
-			if(unidade.getCidade().getId() != null) {
-				unidade2.setCidade(unidade.getCidade());
-			}
-			if(unidade.getCidade().getEstado() == null) {
-				unidade2.getCidade().setEstado(new Estado());
-			}
-			if(unidade.getCidade().getEstado() != null) {
-				unidade2.getCidade().setEstado(unidade.getCidade().getEstado());
-			}
-		}
-		
-		if(unidade.getCodigo() == null || unidade.getCodigo().equals("")) {
-			unidade2.setCodigo("");
-		}
-		
-		if(unidade.getCodigo() != null) {
+
+		if (unidade.getCodigo() != null && !unidade.getCodigo().equals("")) {
 			unidade2.setCodigo(unidade.getCodigo());
 		}
-		
-		if(unidade.getNome() == null || unidade.getNome().equals("")) {
-			unidade2.setNome("");
-		}
-	
 
-		if(unidade.getNome() != null && !unidade.getNome().equals("")) {
+		if (unidade.getNome() != null && !unidade.getNome().equals("")) {
 			unidade2.setNome(unidade.getNome());
 		}
-		if(unidade.getResponsavel() == null || unidade.getResponsavel().equals("")) {
-			unidade2.setResponsavel("");
-		}
-	
-		if(unidade.getResponsavel() != null && unidade.getResponsavel().equals("")) {
-			unidade2.setResponsavel(null);
-		}
-		if(unidade.getResponsavel() != null && !unidade.getResponsavel().equals("")) {
+		if (unidade.getResponsavel() != null && !unidade.getResponsavel().equals("")) {
 			unidade2.setResponsavel(unidade.getResponsavel());
 		}
-		
-		if(unidade.getAtivo() == null || unidade.getAtivo().equals("")) {
-			unidade2.setAtivo("");
-		}
-		
-		if(unidade.getAtivo() != null) {
+
+		if (unidade.getAtivo() != null && !unidade.getAtivo().equals("")) {
 			unidade2.setAtivo(unidade.getAtivo());
 		}
 
-		Example<Unidade> example = Example.of( unidade2, 
-				ExampleMatcher.matching()
-					.withIgnoreCase()
-					.withStringMatcher(StringMatcher.CONTAINING) );
+		if (unidade.getCidade() != null) {
+			if (unidade.getCidade().getId() != null) {
+				unidade2.setCidade(unidade.getCidade());
+				unidade2.getCidade().setId(unidade.getCidade().getId());
+			}
+			if (unidade.getCidade().getEstado() != null) {
+				if (unidade.getCidade().getEstado().getId() != null) {
+					unidade2.getCidade().setEstado(unidade.getCidade().getEstado());
+					unidade2.getCidade().setId(unidade.getCidade().getId());
+				}
+			}
+		}
+		Example<Unidade> example = Example.of(unidade2,
+				ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
 		return unidadeRepository.findAll(example);
 	}
-	
+
 //	@Override
 //	@Transactional(readOnly = true)
 //	public Page<Unidade> buscar(Unidade unidade, Pageable page) {
@@ -183,6 +154,5 @@ public class CadastroUnidadeServiceImpl implements CadastroUnidadeService {
 //					.withStringMatcher(StringMatcher.CONTAINING) );
 //		return unidadeRepository.findAll(example, page);
 //	}
-
 
 }
